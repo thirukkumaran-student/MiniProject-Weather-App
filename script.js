84c70efe091da7b30b51bf8574aa6d20bfcfbf52
thirukkumaran-student <thirukkumaran2410584@ssn.edu.in>
2026-06-14 21:50:48 +0530

    Errors are corrected

--- script.js
+++ script.js
@@ -18,32 +18,47 @@ let data = {
     weather : ''
 }
 let url;
-search.addEventListener('click', () =>
+search.addEventListener('click', (event) =>
 {
+    event.preventDefault();
     data = {
     city : '',
     weatherEmoji : '',
     temperature : '',
     humidity : '',
     weather : ''}
-    
+    console.log("BUTTON CLICKED")
     data.city = cityName.value
-    console.log(data)
-    url = `https://api.openweathermap.org/data/2.5/weather?q=${data.city}&appid=b3d9640f25d425e0f0a964b9b675042d`
+    url = `https://api.openweathermap.org/data/2.5/weather?q=${data.city}&units=metric&appid=6a52dee0aa973a7d5354afccdd96ed57`
+    try{
+        fetch(url)
+    .then(r => r.json())
+    .then(console.log)
+    .catch(console.error);
+    }
+    catch(error){
+        console.log(error)
+    }
     searchWeather(makeChanges)
 })
 
 async function searchWeather(callback){
     let apiData
     try{
+        console.log('before await')
         const p = await fetch(url)
+        console.log('after await')
         apiData = await p.json()
-        // console.log(apiData)
+        console.log(apiData)
     }
     catch(error)
-    {console.log(error)}
+    {
+        console.error(error)
+        console.error(error.message)
+        console.error(error.name)
+    }
 try{
-    data.temperature = `${(Math.round(apiData.main.temp - 273.13)).toString()}°C`
+    data.temperature = `${(apiData.main.temp).toString()}°C`
     data.humidity = `${(apiData.main.humidity).toString()}%`
     data.weather = `${apiData.weather[0].description}`
     // console.log(data)
@@ -106,6 +121,9 @@ catch(error){
     setTimeout(() => {cityName.value = '';
         city.style.fontSize = '2.5rem';
         city.textContent = 'City/Country';
+        temperature.textContent = 'Temperature'
+        humidity.textContent = 'Humidity'
+        weather.textContent = 'Weather'
     }, 3000)
     return
 }
